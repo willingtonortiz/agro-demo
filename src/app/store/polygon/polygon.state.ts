b1fbae9abb290f3fb1fc6cd976eecec8983aa6b1
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2020-01-16 09:57:19 -0500

    Estado del polygono terminado

--- src/app/store/polygon/polygon.state.ts
+++ src/app/store/polygon/polygon.state.ts
@@ -1,24 +1,38 @@
 import { State, Action, StateContext } from "@ngxs/store";
 import { PolygonActions } from './polygon.actions';
+import { AgroApiHttpService } from 'src/app/services/agro-api-http.service';
 
 export interface PolygonStateModel {
 	id: string;
+	agroApiId: string;
 	name: string;
 	coordinates: Array<any>;
 };
 
 @State<PolygonStateModel>({
-	name: "polygon",
+	name: "Polygon",
 	defaults: {
 		id: "",
 		name: "",
-		coordinates: []
+		coordinates: [],
+		agroApiId: "",
 	}
 })
 export class PolygonState {
 
-	@Action(PolygonActions.CreatePolygon)
-	createPolygon({ }: StateContext<PolygonStateModel>, { }: PolygonActions.CreatePolygon) {
+	public constructor(private readonly agroApiHttpService: AgroApiHttpService) {
+	}
 
+	@Action(PolygonActions.SetProperty)
+	public setProperty({ patchState, }: StateContext<PolygonStateModel>, { properties }: PolygonActions.SetProperty) {
+		console.log(properties);
+		patchState({
+			...properties
+		});
+	}
+
+	@Action(PolygonActions.CreatePolygon)
+	createPolygon({ getState }: StateContext<PolygonStateModel>, { }: PolygonActions.CreatePolygon) {
+		console.log(getState());
 	}
 }
